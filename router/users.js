17c05209b420c0a2d9eb14fcf4d44b31a29e4e58
haris <89023322+Haresz@users.noreply.github.com>
2022-11-03 08:21:20 +0700

    first commit

---
+++ router/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router()
+
+const useController = require('../controllers/user')
+
+router.get('/users', useController.index )
+
+router.post('/user', useController.store)
+
+router.put('/user/:id', useController.update)
+
+router.delete('/user/:id', useController.delete)
+
+module.exports = router
